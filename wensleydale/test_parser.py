83e2985eac7c367cbb0b4de1cd41c307a987097d
Efron Licht <efron.python@gmail.com>
2017-05-22 13:38:29 -0700

    git add test

--- wensleydale/test_parser.py
+++ wensleydale/test_parser.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import ast
+from ast import parse
 from wensleydale import parser
 
-
+def dparse(string):
+    return parser.dictify(parse(string))
 def test_int():
     '''
     An int object is properly decoded.
     '''
-    # Create fake data.
-    obj = ast.parse('a = 1')
+    int_ast = dparse('1')
+    assert int_ast['body'][0]['lineno'] == 1
 
-    # Run the test.
-    result = parser.dictify(obj)
+def test_whitespacing_creates_different_asts():
+    'column offset should be preserved in asts'
+    properly_whitespaced_loop = dparse('for n in range(5):\n    print(n)'))
+    improperly_whitespaced_loop = dparse('for n in range(5):\n print(n)'))
+    assert properly_whitespaced_loop != improperly_whitespaced_loop
 
-    # Check the result.
-    assert result['body'][0]['lineno'] == 1
+def shadowing_creates_different_asts():
+    """ a class which shadows the AST internal name of a built-in should have a distinct abstract syntax tree
+    from the built-in it shadows """
+    shadowed_int = dparse('Num(1)')
+    normal_int = dparse('1')
+    assert shadowed_int != normal_int
